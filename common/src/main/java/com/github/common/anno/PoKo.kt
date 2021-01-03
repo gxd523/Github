15af3e48ee8a654b59507f80cb51e3bcf62b5cdc
gxd <gxd523@gmail.com>
2021-01-03 17:26:36 +0800

    noarg、allopen

--- common/src/main/java/com/github/common/anno/PoKo.kt
+++ common/src/main/java/com/github/common/anno/PoKo.kt
@@ -1,5 +1,6 @@
 package com.github.common.anno
 
+// TODO: 1/3/21 重点：配合noArg(给Data Class添加无参构造函数)、allOpen(去掉Data Class的final修饰符，使之可以被继承)
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS)
 annotation class PoKo
