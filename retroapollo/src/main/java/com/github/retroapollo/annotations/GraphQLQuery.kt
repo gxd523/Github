ddcc9a3c26945cd886a067db84c5f3556821fa9f
gxd <gxd523@gmail.com>
2021-01-29 10:28:08 +0800

    no message

--- retroapollo/src/main/java/com/github/retroapollo/annotations/GraphQLQuery.kt
+++
@@ -1,7 +0,0 @@
-package com.github.retroapollo.annotations
-
-import kotlin.annotation.AnnotationRetention.RUNTIME
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(RUNTIME)
-annotation class GraphQlQuery(val value: String)
